293dc2dd8c214688a5d35abec860f79ba1ce038c
Davian Canty <davian49@hotmail.com>
2023-04-10 23:28:21 -0400

    removed authHeader

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 //import authHeader from './auth-header';
 
-const API_URL = 'https://www.bmclive.app/test/';
+const API_URL = 'https://api.bmclive.app/test/';
 
 class UserService {
   getPublicContent() {
